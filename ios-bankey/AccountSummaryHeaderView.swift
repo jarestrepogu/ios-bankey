82cb0fcd443fd41fcfa4468bac6461329d02abf4
Jorge Restrepo <jorge.restrepo@mercadolibre.com.co>
2022-10-28 19:11:47 -0500

    feature: Add networking to AccountSummary.

--- ios-bankey/AccountSummaryHeaderView.swift
+++ ios-bankey/AccountSummaryHeaderView.swift
@@ -10,7 +10,21 @@ import UIKit
 class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
     let shakeyBell = ShakeyBellView()
+    
+    struct ViewModel {
+        let welcomeMessage: String
+        let name: String
+        let date: Date
+        
+        var dateFormatted: String {
+            return date.monthDayYearString
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -49,5 +63,11 @@ class AccountSummaryHeaderView: UIView {
             shakeyBell.trailingAnchor.constraint(equalTo: trailingAnchor),
             shakeyBell.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+    
+    func configure(viewModel: ViewModel) {
+        welcomeLabel.text = viewModel.welcomeMessage
+        nameLabel.text = viewModel.name
+//        dateLabel.text = viewModel.dateFormatted
     }
 }
